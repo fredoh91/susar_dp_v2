f514d42721d5a53160c5c186755a53dc01cd64b5
fredoh91 <fredoh@gmail.com>
2021-05-06 23:59:26 +0200

    liens CIOMS fonctionnels

---
+++ public/open_cioms.php
@@ -0,0 +1,20 @@
+<?php
+
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+
+if (isset($_GET['link'])) 
+    {
+	$file = $_GET['link'];
+	if (file_exists($file)) {
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="'.basename($file).'"');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($file));
+		readfile($file);
+		exit;
+	}
+}
+?>
